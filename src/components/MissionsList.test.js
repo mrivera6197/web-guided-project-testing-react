6f28325f254d5b0729a04033094aaa514e4e7130
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-11-09 14:01:19 -0500

    lecture work-in-progress

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -13,10 +13,12 @@ const missions = [
 
 test("rerenders correctly when passing in new mission data", ()=>{
     const {rerender} = render(<MissionsList missions={[]}/>);
+    
     let missionObjects = screen.queryAllByTestId("mission");
     expect(missionObjects).toStrictEqual([]);
 
     rerender(<MissionsList missions={missions}/>);
+
     missionObjects = screen.queryAllByTestId("mission");
     expect(missionObjects).toHaveLength(2);
 });
