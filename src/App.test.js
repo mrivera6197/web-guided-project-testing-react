dc7501cf31fbf952951f8650d3e001ec66d27b50
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-11-09 13:16:18 -0500

    lecture work-in-progress

---
+++ src/App.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {fireEvent, render, screen, waitFor} from "@testing-library/react";
+import App from './App';
+
+test("render without errors", ()=>{
+    render(<App/>);
+});
